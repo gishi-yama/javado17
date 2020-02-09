2803686ab1ba480cbf9c3aa631bd2189384cc914
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-02-09 10:44:13 +0900

    adjust record

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+record Person(String name, int age) {};
+
 public class Main {
 
   public static void main(String[] args) {
@@ -26,6 +28,4 @@ public class Main {
   }
 
 }
-
-record Person(String name, int age) {};
 
