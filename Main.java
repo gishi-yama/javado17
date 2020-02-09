a3b1f6d23334958848afde0044835e8080a3c3bc
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-02-09 10:36:30 +0900

    Adjust indents

--- Main.java
+++ Main.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        var entries = new ArrayList<Person>();
+  public static void main(String[] args) {
+     var scanner = new Scanner(System.in);
+     var entries = new ArrayList<Person>();
 
-        while(true) {
-          System.out.print("Who are you?: ");
-          var who = scanner.next();
-          System.out.print("How old are you?: ");
-          var howOld = scanner.nextInt();
+     while(true) {
+       System.out.print("Who are you?: ");
+       var who = scanner.next();
+       System.out.print("How old are you?: ");
+       var howOld = scanner.nextInt();
 
-          var someOne = new Person(who, howOld);
-          if(!entries.contains(someOne)) {
-              entries.add(someOne);
-          } else {
-              System.out.println(someOne.name() + " is already entried.");
-          }
+       var someOne = new Person(who, howOld);
+       if(!entries.contains(someOne)) {
+          entries.add(someOne);
+       } else {
+          System.out.println(someOne.name() + " is already entried.");
+       }
 	  
-          entries.stream()
-                    .sorted(Comparator.comparingInt(Person::age))
-                    .forEach(System.out::println);
-        }
-    }
+       entries.stream()
+         .sorted(Comparator.comparingInt(Person::age))
+         .forEach(System.out::println);
+     }
+  }
 
 }
 
